72100eafe2df2d814bde5227767db1b10e32a5f2
TroPiczMoDz <tropiczmodz@outlook.fr>
2022-02-17 18:21:48 +0100

    changes reee

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 import { PostCard, PostWidget } from '../components';
 import { getPosts } from '../services'
-import MetaTags from 'react-meta-tags'; 
+import Helmet from 'react-helmet'
 
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <MetaTags>
+      <Helmet>
         <title>HomePage</title>
         <meta content="Embed Title" property="og:title" />
         <meta content="Site Description" property="og:description" />
         <meta content="https://blog-graph.vercel.app" property="og:url" />
         <meta content="https://cdn.donmai.us/sample/5d/2f/__kitagawa_marin_sono_bisque_doll_wa_koi_wo_suru_drawn_by_nonbire__sample-5d2f62793c8328d6e94e12a1a0cdc679.jpg" property="og:image" />
         <meta content="#43B581" data-react-helmet="true" name="theme-color" />
-      </MetaTags>
+      </Helmet>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
