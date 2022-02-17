8de3e25e3393e42e7035b53fea98f74ae4f39f0c
TroPiczMoDz <tropiczmodz@outlook.fr>
2022-02-17 18:06:07 +0100

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from 'next/head'
 import { PostCard, PostWidget } from '../components';
-import { getPosts } from '../services' 
+import { getPosts } from '../services'
+import MetaTags from 'react-meta-tags'; 
 
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
+      <MetaTags>
+        <title>Home</title>
+        <meta name="description" content="Some description." />
+        <meta property="og:title" content="Home" />
+        <meta property="og:image" content="https://cdn.donmai.us/sample/5d/2f/__kitagawa_marin_sono_bisque_doll_wa_koi_wo_suru_drawn_by_nonbire__sample-5d2f62793c8328d6e94e12a1a0cdc679.jpg" />
+      </MetaTags>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
