fb28c1957eb879592c0197461ffb7e9fef755701
TroPiczMoDz <tropiczmodz@outlook.fr>
2022-02-17 18:15:48 +0100

    changeees

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,12 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <MetaTags>
-        <title>Home</title>
-        <meta name="description" content="Some description." />
-        <meta property="og:title" content="Home" />
-        <meta property="og:image" content="https://cdn.donmai.us/sample/5d/2f/__kitagawa_marin_sono_bisque_doll_wa_koi_wo_suru_drawn_by_nonbire__sample-5d2f62793c8328d6e94e12a1a0cdc679.jpg" />
+        <title>HomePage</title>
+        <meta content="Embed Title" property="og:title" />
+        <meta content="Site Description" property="og:description" />
+        <meta content="https://blog-graph.vercel.app" property="og:url" />
+        <meta content="https://cdn.donmai.us/sample/5d/2f/__kitagawa_marin_sono_bisque_doll_wa_koi_wo_suru_drawn_by_nonbire__sample-5d2f62793c8328d6e94e12a1a0cdc679.jpg" property="og:image" />
+        <meta content="#43B581" data-react-helmet="true" name="theme-color" />
       </MetaTags>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
